f81ca0fa8df3b7b2c7cd469e23f921581f7d6cca
Monica Alyssa <monica_alyssa@icloud.com>
2024-11-13 16:49:23 -0500

    Add podcast player

---
+++ src/components/Podcast/PodtcastPlayer.jsx
@@ -0,0 +1,11 @@
+function PodcastPlayer() {
+
+    return (
+        <audio controls>
+            <source src="" type="audio/mp3"></source>
+            Hello
+        </audio>
+    )
+}
+
+export default PodcastPlayer;
