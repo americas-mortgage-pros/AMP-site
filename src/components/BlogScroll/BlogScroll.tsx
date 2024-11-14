56398fd30c938df0acc3d4d556d9ac8b82518258
americas-mortgage-pros <monicawilliams@amprefi.com>
2024-11-14 12:27:10 -0500

    Fix typescript errors

--- src/components/BlogScroll/BlogScroll.tsx
+++ src/components/BlogScroll/BlogScroll.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Carousel } from "@mantine/carousel";
 import { Box, Button, Container, Flex, SegmentedControl, Text, Title } from "@mantine/core";
 import BlogCard from "../Blog/BlogCard";
